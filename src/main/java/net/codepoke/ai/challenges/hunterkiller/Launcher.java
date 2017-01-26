d9dd74868c656148922606543b4ee5ec2875cae8
antonvalkenberguni <a.valkenberg@student.maastrichtuniversity.nl>
2017-01-26 18:50:31 +0100

    First commit with testbots

---
+++ src/main/java/net/codepoke/ai/challenges/hunterkiller/Launcher.java
@@ -0,0 +1,114 @@
+package net.codepoke.ai.challenges.hunterkiller;
+
+import java.net.URISyntaxException;
+
+import net.codepoke.ai.GameRules;
+import net.codepoke.ai.GameRules.Result;
+import net.codepoke.ai.challenge.hunterkiller.HunterKillerAction;
+import net.codepoke.ai.challenge.hunterkiller.HunterKillerRules;
+import net.codepoke.ai.challenge.hunterkiller.HunterKillerState;
+import net.codepoke.ai.challenge.hunterkiller.HunterKillerStateFactory;
+import net.codepoke.ai.challenges.hunterkiller.bots.RandomBot;
+import net.codepoke.ai.challenges.hunterkiller.bots.TestBot;
+import net.codepoke.ai.network.MatchMessageParser;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Json;
+
+public class Launcher {
+
+	public static void main(String[] arg) throws URISyntaxException {
+
+		// Create the packed asset atlas
+		TexturePacker.Settings settings = new TexturePacker.Settings();
+		settings.maxHeight = 2048;
+		settings.maxWidth = 2048;
+		settings.useIndexes = true;
+		settings.paddingX = settings.paddingY = 1;
+		settings.edgePadding = true;
+		settings.bleed = true;
+		settings.filterMin = TextureFilter.MipMapNearestNearest;
+		settings.filterMag = TextureFilter.MipMapNearestNearest;
+		// TexturePacker.process(settings, "imgs/", System.getProperty("user.dir"), "game.atlas");
+
+		// Start up the game
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.forceExit = true;
+
+		final HunterKillerVisualization listener = new HunterKillerVisualization();
+
+		new LwjglApplication(listener, config);
+
+		// testStream(listener, "localhost:9000/competition/stream_match" , "HunterKiller" ,
+		// "HunterKiller-glv3jsudnt9gbuvavqtesuhl5k-6");
+		simulateStream(listener);
+	}
+
+	/**
+	 * Tests streaming a match to the visualizer by locally playing a game, and sending it to the
+	 * visualizer.
+	 * 
+	 * @param listener
+	 */
+	private static void simulateStream(final HunterKillerVisualization vis) {
+		final MatchMessageParser<HunterKillerState, HunterKillerAction> listener = vis.getParser();
+		new Thread() {
+
+			public void run() {
+
+				// Small wait for visualisation to properly setup
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				GameRules<HunterKillerState, HunterKillerAction> rules = new HunterKillerRules();
+				Array<HunterKillerAction> actions = new Array<HunterKillerAction>();
+
+				// Create the initial state
+				HunterKillerState state = new HunterKillerStateFactory().generateInitialState(new String[] { "A", "B", "C", "D" }, null);
+
+				HunterKillerState orgState = state.copy();
+
+				RandomBot randomBot = new RandomBot(); // Instantiate your bot here
+				// RulesBot rulesBot = new RulesBot();
+				TestBot testBot = new TestBot(vis);
+
+				Json json = new Json();
+
+				listener.parseMessage(vis.getLastState(), json.toJson(state.getPlayers())); // Players
+				listener.parseMessage(vis.getLastState(), json.toJson(Array.with(orgState))); // Initial State
+
+				// The following snippet will run a match with the given AI for all player seats until the match is
+				// finished or an error occurs.
+				// This would fail if your AI assumes it always operates in the same seat; instead of querying
+				// State.getCurrentPlayer().
+				Result result;
+				do {
+					HunterKillerAction action;
+					// Only have player 'A' play as a rules-bot
+					if (state.getActivePlayer()
+								.getName() == "A") {
+						action = testBot.handle(state);
+					} else {
+						action = randomBot.handle(state);
+					}
+
+					actions.add(action);
+					// Alternatively, send the action immediately: listener.parseMessage(vis.getLastState(),
+					// json.toJson(action));
+					result = rules.handle(state, action);
+				} while (!result.isFinished() && result.isAccepted());
+
+				listener.parseMessage(vis.getLastState(), json.toJson(actions));
+
+			}
+		}.start();
+	}
+
+}
---
+++ src/main/java/net/codepoke/ai/challenges/hunterkiller/bots/RandomBot.java
@@ -0,0 +1,110 @@
+package net.codepoke.ai.challenges.hunterkiller.bots;
+
+import java.util.List;
+import java.util.Random;
+
+import net.codepoke.ai.challenge.hunterkiller.HunterKillerAction;
+import net.codepoke.ai.challenge.hunterkiller.HunterKillerState;
+import net.codepoke.ai.challenge.hunterkiller.Map;
+import net.codepoke.ai.challenge.hunterkiller.MoveGenerator;
+import net.codepoke.ai.challenge.hunterkiller.Player;
+import net.codepoke.ai.challenge.hunterkiller.enums.UnitOrderType;
+import net.codepoke.ai.challenge.hunterkiller.enums.UnitType;
+import net.codepoke.ai.challenge.hunterkiller.gameobjects.mapfeature.MapFeature;
+import net.codepoke.ai.challenge.hunterkiller.gameobjects.mapfeature.Structure;
+import net.codepoke.ai.challenge.hunterkiller.gameobjects.unit.Unit;
+import net.codepoke.ai.challenge.hunterkiller.orders.StructureOrder;
+import net.codepoke.ai.challenge.hunterkiller.orders.UnitOrder;
+import net.codepoke.ai.network.AIBot;
+
+/**
+ * Represents an {@link AIBot} for the HunterKiller game that generates random orders for it's structures and units.
+ * 
+ * @author Anton Valkenberg (anton.valkenberg@gmail.com)
+ *
+ */
+public class RandomBot
+		extends AIBot<HunterKillerState, HunterKillerAction> {
+
+	private static Random r = new Random();
+	private static final double noUnitOrderThreshold = 0.2;
+	private static final double noBaseOrderThreshold = 0.1;
+
+	public RandomBot() {
+		super("", HunterKillerState.class, HunterKillerAction.class);
+	}
+
+	public HunterKillerAction handle(HunterKillerState state) {
+		// Create a random action
+		HunterKillerAction random = new HunterKillerAction(state);
+		Player player = state.getPlayer(state.getCurrentPlayer());
+
+		// Move through all structure
+		for (Structure structure : player.getStructures(state.getMap())) {
+			if (r.nextDouble() <= noBaseOrderThreshold)
+				continue;
+
+			// Get all legal orders for this structure
+			List<StructureOrder> legalOrders = MoveGenerator.getAllLegalOrders(state, structure);
+
+			// Add a random order
+			if (!legalOrders.isEmpty()) {
+				random.addOrder(legalOrders.get(r.nextInt(legalOrders.size())));
+			}
+		}
+
+		Map map = state.getMap();
+
+		// Move through all Units
+		for (Unit unit : player.getUnits(state.getMap())) {
+			// Check if we want to do nothing
+			if (r.nextDouble() <= noUnitOrderThreshold)
+				continue;
+
+			// Get all legal rotation orders for this unit
+			List<UnitOrder> legalRotationOrders = MoveGenerator.getAllLegalOrders(state, unit, true, false, false);
+			// Get all legal move orders for this unit
+			List<UnitOrder> legalMoveOrders = MoveGenerator.getAllLegalOrders(state, unit, false, true, false);
+			// Get all legal attack orders for this unit
+			List<UnitOrder> legalAttackOrders = MoveGenerator.getAllLegalOrders(state, unit, false, false, true);
+
+			// Remove all attacks without a proper target
+			legalAttackOrders.removeIf((order) -> {
+				Unit target = map.getUnitAtLocation(order.getTargetLocation());
+				MapFeature feature = map.getFeatureAtLocation(order.getTargetLocation());
+				return target == null && !(feature instanceof Structure);
+			});
+
+			// Remove all attack with an ally base as target
+			legalAttackOrders.removeIf(order -> {
+				MapFeature feature = map.getFeatureAtLocation(order.getTargetLocation());
+				return feature instanceof Structure && ((Structure) feature).getControllingPlayerID() == unit.getControllingPlayerID();
+			});
+
+			// Remove all attacks with an ally unit as target, unless the order is a Medic's special attack
+			legalAttackOrders.removeIf(order -> {
+				Unit target = map.getUnitAtLocation(order.getTargetLocation());
+				return target != null && target.getControllingPlayerID() == unit.getControllingPlayerID()
+						&& !(order.getUnitType() == UnitType.Medic && order.getOrderType() == UnitOrderType.ATTACK_SPECIAL);
+			});
+
+			double attackType = r.nextDouble();
+			// Do a random rotation with 20% chance
+			if (attackType <= 0.2 && !legalRotationOrders.isEmpty()) {
+				random.addOrder(legalRotationOrders.get(r.nextInt(legalRotationOrders.size())));
+			}
+			// Do a random move with 50% chance
+			else if (attackType <= 0.7 && !legalMoveOrders.isEmpty()) {
+				random.addOrder(legalMoveOrders.get(r.nextInt(legalMoveOrders.size())));
+			}
+			// Do a random attack with 30% chance
+			else if (!legalAttackOrders.isEmpty()) {
+				random.addOrder(legalAttackOrders.get(r.nextInt(legalAttackOrders.size())));
+			}
+		}
+
+		// Return the randomly created action
+		return random;
+	}
+
+}
